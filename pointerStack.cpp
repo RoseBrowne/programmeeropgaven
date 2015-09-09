0ead78dc8e5db8006384e93d65637ce59621881d
Patrick Bergman <p.m.bergman@umail.leidenuniv.nl>
2015-09-09 12:54:50 +0200

    Add implementation of pointerstack

--- pointerStack.cpp
+++ pointerStack.cpp
@@ -7,3 +7,43 @@
 //
 
 #include "pointerStack.h"
+
+// creates a new stack as double linked list
+void create() {
+    top = NULL;
+}
+
+bool isEmpty() {
+    return top == NULL;
+}
+
+void clear() {
+    if(top != NULL) {
+        while(top != NULL)
+        {
+            pop(false);
+        }
+    }
+    cout << "Stapel is geleegd" << endl;
+}
+
+void push(const T& newItem, bool& success) {
+    np = new node;
+    np->value = newItem;
+    np->next = top;
+    top = np;
+}
+
+void pop(bool& success) {
+    old = top;
+    top = top->next;
+    delete old;
+    success = true;
+}
+
+void top(T& topItem, bool& success) {
+    if(top != NULL) {
+        topItem = top->value;
+    }
+    success = top != NULL;
+}
--- pointerStack.h
+++ pointerStack.h
@@ -9,6 +9,26 @@
 #ifndef __Stacks__pointerStack__
 #define __Stacks__pointerStack__
 
-#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+template <class T>
+struct node {
+    T value;
+    node *next;
+}
+
+template <class T>
+class pointerStack {
+public:
+    void create();
+    bool isEmpty();
+    void clear();
+    void push(T& newItem, bool& success);
+    void pop(bool& success);
+    void top(T& topItem, bool& success);
+private:
+    *top;
+};
 
 #endif /* defined(__Stacks__pointerStack__) */
